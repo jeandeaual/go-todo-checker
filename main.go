7701e50abee23561e75b56e3b6ffa8b4bc082d31
Alexis Jeandeau <alexis.jeandeau@gmail.com>
2019-04-09 21:25:43 +0900

    Add comments

--- main.go
+++ main.go
@@ -13,16 +13,19 @@ import (
 
 const pattern = "TODO"
 
+// importPkg imports a Go package from a path
 func importPkg(path, dir string) (*build.Package, error) {
 	pkg, err := build.Import(path, dir, build.ImportComment)
 	if err != nil {
 		return nil, err
 	}
 
+	// Don't parse binary-only packages (they don't contain any comment)
 	if pkg.BinaryOnly {
 		return nil, fmt.Errorf("package %s is binary-only", path)
 	}
 
+	// Don't parse command packages
 	if pkg.IsCommand() {
 		return nil, fmt.Errorf("package %s is a command", path)
 	}
@@ -30,7 +33,9 @@ func importPkg(path, dir string) (*build.Package, error) {
 	return pkg, nil
 }
 
+// extractTODO extracts comments containing TODO from a Go source file
 func extractTODO(fname string) error {
+	// Parse the file and create the AST
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, fname, nil, parser.ParseComments)
 	if err != nil {
@@ -39,9 +44,12 @@ func extractTODO(fname string) error {
 
 	cmap := ast.NewCommentMap(fset, f, f.Comments)
 	for n, cgs := range cmap {
+		// Retrieve the file pointer
 		file := fset.File(n.Pos())
+		// Iterate through the comment groups
 		for _, cg := range cgs {
 			text := cg.Text()
+			// Check if the comment group contains the pattern
 			if strings.Contains(text, pattern) {
 				fmt.Printf("%s:%v:\n%s\n", fname, file.Position(cg.Pos()).Line, text)
 			}
